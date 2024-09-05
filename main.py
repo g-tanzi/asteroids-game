9195d04aec298d4a3667231384a7af86a7803abf
g-tanzi <giovannipaolo.tanzi@gmail.com>
2024-09-05 16:24:06 -0400

    Added FPS, Sprite and Ship

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     print("Starting asteroids!")
@@ -7,7 +9,10 @@ def main():
     print("Screen height:", SCREEN_HEIGHT)
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         # Listen for close game windoe event
@@ -15,8 +20,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill('black')
+        screen.fill("black")
+        player.draw(screen)
+
+        # Updates the actual screen
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
